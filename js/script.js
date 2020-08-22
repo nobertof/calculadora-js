3ce4872fe6253d074ac43827419ab5a89c6fbeba
Noberto filho <francisco.noberto.gomes@gmail.com>
2020-08-22 11:41:08 -0300

    calculadora com js feita

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function calcular(tipo,valor){
+    if(tipo === 'acao'){
+        if(valor === 'c'){
+            //limpar o visor
+            document.getElementById('resultado').value='';
+        }
+        if(valor === '+' || valor === '-' || valor === '*' || valor === '/' || valor === '.'){
+            document.getElementById('resultado').value+=valor;
+        }
+        if(valor === '='){
+            var valor_campo = document.getElementById('resultado').value;
+            document.getElementById('resultado').value = eval(valor_campo);
+        }
+    }else if(tipo === 'valor'){
+        document.getElementById('resultado').value += valor;
+    }
+}
